2dffdf9912545c3ed50749ddcbc7606d1ea6fc56
Richard Beckett <rbeckett@scottlogic.co.uk>
2016-05-09 13:18:45 +0100

    simplified adding a result

--- server/dialogs/index.js
+++ server/dialogs/index.js
@@ -16,35 +16,18 @@ dialog.on('Help', builder.DialogAction.send(prompts.helpMessage));
 dialog.on('AddResult', [
 	function (session, args, next) {
 		// See if got the tasks title from our LUIS model.
-		var p1 = builder.EntityRecognizer.findEntity(args.entities, 'player::p1');
-		var p2 = builder.EntityRecognizer.findEntity(args.entities, 'player::p2');
-		var s1 = builder.EntityRecognizer.findEntity(args.entities, 'score::s1');
-		var s2 = builder.EntityRecognizer.findEntity(args.entities, 'score::s2');
+		let p1 = builder.EntityRecognizer.findEntity(args.entities, 'player::p1');
+		let p2 = builder.EntityRecognizer.findEntity(args.entities, 'player::p2');
+		let s1 = builder.EntityRecognizer.findEntity(args.entities, 'score::s1');
+		let s2 = builder.EntityRecognizer.findEntity(args.entities, 'score::s2');
 
 		//TODO deal with the above not being found
-
-
-		// pass the result to the next step.
-		next({
-			p1: p1.entity,
-			p2: p2.entity,
-			s1: s1.entity,
-			s2: s2.entity
-		});
-	},
-	function (session, results) {
-		// Save the result
-		if (!results.p1 || !results.p2 || isNaN(results.s1) || isNaN(results.s2)) {
-			session.send(prompts.error);
+		if (!p1 || !p2 || !s1 || !s2) {
+			session.send(prompts.notEnoughData);
 		}
+		// got all 4 pieces of data process it
 		else {
-			var res = results.p1 +' '+ results.s1 + ' - ' + results.s2 + ' ' + results.p2;
-			console.log(res);
-			if (!session.userData.results) {
-				session.userData.results = [res];
-			} else {
-				session.userData.results.push(res);
-			}
+			var res = p1.entity +' '+ s1.entity + ' - ' + s2.entity + ' ' + p2.entity;
 			session.send(prompts.resultCreated, { result: res });
 		}
 	}
