9e048f885c5efe544dcb86ffb4a622dd4ad82d79
Richard Beckett <rbeckett@scottlogic.co.uk>
2016-05-12 10:01:35 +0100

    channel persists user data when slackbot starts

--- server/slackbot.js
+++ server/slackbot.js
@@ -3,13 +3,15 @@
 var Botkit = require('botkit');
 var builder = require('botbuilder');
 var index = require('./dialogs/index');
+var Controller = require('./controller/controller');
+var appController = new Controller();
 
-var controller = Botkit.slackbot();
-var bot = controller.spawn({
+var botController = Botkit.slackbot();
+var bot = botController.spawn({
 	token: require('./config').slackBotToken
 });
 
-var slackBot = new builder.SlackBot(controller, bot);
+var slackBot = new builder.SlackBot(botController, bot);
 slackBot.add('/', index);
 
 slackBot.add('/say', function(session, message) {
@@ -22,20 +24,33 @@ bot.startRTM(function(err,bot,payload) {
 	if (err) {
 		throw new Error('Could not connect to Slack');
 	}
+	let users = [];
+	payload.users.forEach(function(user) {
+		if (!user.is_bot && user.name !== 'slackbot') {
+			users.push(
+				{
+					id: user.id,
+					name: user.name,
+					fname: user.profile.first_name,
+				}
+			);
+		}
+	});
+	appController.addUsers(users);
 });
 
-slackBot.add('/say', function(session, message) {
-	session.endDialog(message);
-})
 
 ////// Exportable functions /////////
 
-/* To send a single messasge
-channel is of the form 'D16BDMBGB' or 'C16CH0SNQ' and
-message is a string.
+/* To get the user information from Slack
 */
-module.exports.sendMessage = function(channel, message) {
-	slackBot.beginDialog({ channel: channel}, '/say', message);
+module.exports.getUser = function (id, callback) {
+	bot.api.users.info({user: id}, function(err, data) {
+		if (err) {
+            bot.botkit.log('Failed to find user info :(',err);
+        }
+		callback(data);
+	});
 }
 
 /* To send a single messasge
