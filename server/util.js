6d2db27b62ab023bc6b6f48960df6cae94cf401d
Richard Beckett <rbeckett@scottlogic.co.uk>
2016-05-17 13:53:00 +0100

    added util function to change words to numbers

--- server/util.js
+++ server/util.js
@@ -1,6 +1,21 @@
 'use strict';
 const prompts = require('./prompts')
 
+let numbers = {
+    nil: 0,
+    zero: 0,
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    nine: 9,
+    ten: 10
+}
+
 let capWords = function(s) {
     return s.toLowerCase().replace( /\b./g, function(a){ return a.toUpperCase(); } );
 }
@@ -40,5 +55,18 @@ module.exports = {
         }
 
         return playerDoc;
+    },
+
+    convertWordToNumber(word) {
+        console.log(word);
+        if (isNaN(parseInt(word, 10))) {
+            if(numbers.hasOwnProperty(word)) {
+                return numbers[word.toLowerCase()];
+            }
+            return null;
+        }
+        else {
+            return parseInt(word, 10);
+        }
     }
 };
