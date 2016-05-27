6059bd330cf536eb2231a52645962d959ad25296
Andrew Jones <amjones@scottlogic.co.uk>
2016-05-27 10:12:14 +0100

    added winner to bracket

--- src/components/knockout-stage/bracket-column.js
+++ src/components/knockout-stage/bracket-column.js
@@ -7,6 +7,7 @@ var bracketColumn = React.createClass({
 	render: function() {
 		let elements = [];
 		let count = this.props.matches.length;
+		let date = this.props.date;
 
 		this.props.matches.map(function(match, index, array) {
 			let team1Winner = '';
@@ -20,17 +21,17 @@ var bracketColumn = React.createClass({
 
 			elements.push(
 				<div className={'ele c'+count+' c'+count+'e'+index} key ={'c'+count+'e'+index}>
-					<div className={'team game-top '+team1Winner}> <div>{match.team1.country}</div> <div className='score'>{match.result.score1}</div> </div>
+					<div className={'team game-top '+team1Winner}> <div className='country'>{match.team1.country}</div> <div className='score'>{match.result.score1}</div> </div>
 					<div className='upperLine'></div>
 					<div className='lowerLine'></div>
-					<div className={'team game-bottom '+team2Winner}> <div>{match.team2.country}</div> <div className='score'>{match.result.score2}</div> </div>
+					<div className={'team game-bottom '+team2Winner}> <div className='country'>{match.team2.country}</div> <div className='score'>{match.result.score2}</div> </div>
 				</div>
 			);
 		});
 		return (
 			<div className='bracket-column'>
 				<div className='bracket-header'>
-					<h3 className='column-date'>16th-18th JUNE</h3>
+					<h3 className='column-date'>{date}</h3>
 				</div>
 				<div className='bracket-body'>
 					{elements}
